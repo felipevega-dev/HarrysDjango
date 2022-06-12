38766045c5a740e8c7b3c926c757c68416fd57b9
Felipe Vega <feli.vega@duocuc.cl>
2022-06-12 04:07:00 -0400

    AHORA SI XD carrito funciona
    Carrito funciona por fin, el unico problema es que al iniciar sesion con usuario y agregar productos todo bien, pero a la hora de salir de la sesion y volver a agregar productos directamente da error, pero si presionamos el boton de "limpiar" antes de agregar no da ningun error.

--- core/context_processor.py
+++ core/context_processor.py
@@ -4,4 +4,4 @@ def total_carrito(request):
         if "carrito" in request.session.keys():
             for key, value in request.session["carrito"].items():
                 total += int(value["acumulado"])
-    return {"total_carrito:": total}
+    return {"total_carrito": total}
